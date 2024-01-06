72a91c28d70fa0e8840a2591aa8643a680743be4
alexis martinez martinez <al3x1612@gmail.com>
2024-01-05 19:54:30 -0600

    coment

---
+++ src/main/java/com/example/ejemindat2/Ejemindat2Application.java
@@ -0,0 +1,13 @@
+package com.example.ejemindat2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejemindat2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejemindat2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ejemindat2/Ejemindat2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejemindat2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejemindat2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
